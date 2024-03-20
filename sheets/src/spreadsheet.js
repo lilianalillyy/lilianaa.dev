fd1746fc1c65a80ac6b81c74e2a143350494011a
Mia Pilchová <valisova@lilianaa.dev>
2024-03-20 09:01:27 +0000

    refactor(sheets): add `getCachedSpreadsheetData`

--- sheets/src/spreadsheet.js
+++ sheets/src/spreadsheet.js
@@ -8,48 +8,48 @@ const { TEMP_DIR } = require("./constants");
 const decompress = require("decompress");
 const path = require("path");
 
-module.exports.getSpreadsheetData = async (spreadsheetId) => {
+const getSpreadsheetData = async (spreadsheetId) => {
     let zipFile = null;
     let tempDir = null;
     try {
         const sheet = new GoogleSpreadsheet(spreadsheetId, googleAuthClient);
         await sheet.loadInfo(true);
-    
+
         const uuid = randomUUID();
-    
+
         zipFile = path.join(TEMP_DIR, `${uuid}.zip`);
-    
+
         tempDir = path.join(TEMP_DIR, uuid);
         mkdirSync(tempDir);
-    
+
         const zipStream = await sheet.downloadAsZippedHTML(true);
         await streamToFile(zipStream, zipFile);
-    
+
         await decompress(zipFile, tempDir);
-    
+
         let sheets = {};
-    
+
         const htmlFilesPath = path.join(tempDir, "*.html");
         const htmlFiles = glob(htmlFilesPath);
-    
+
         for (const htmlFile of htmlFiles) {
             const name = path.basename(htmlFile, path.extname(htmlFile))
             const html = readFileSync(htmlFile, "utf-8");
-    
+
             sheets[name] = {
                 html,
             };
         }
-    
+
         const cssPath = path.join(tempDir, "resources", "sheet.css");
         let css = null;
-    
+
         if (existsSync(cssPath)) {
             css = readFileSync(cssPath, "utf-8");
         }
-    
+
         await cleanupTemp(zipFile, tempDir);
-    
+
         const data = {
             title: spreadsheetId,
             sheets,
@@ -59,6 +59,34 @@ module.exports.getSpreadsheetData = async (spreadsheetId) => {
         return data
     } catch (e) {
         await cleanupTemp(zipFile, tempDir)
-        console.log(`failed to fetch spreadsheet ${spreadsheetId}`, {e});
+        console.log(`failed to fetch spreadsheet ${spreadsheetId}`, { e });
     }
+}
+
+const getCachedSpreadsheetData = async (cache, spreadsheetId, { fetchIfNotFound = true, filterData = (data) => data } = {}) => {
+    let data = null;
+
+    if (cache) {
+        let cachedData = (await cache.get(`spreadsheet:${spreadsheetId}`))?.item;
+
+        if (cachedData) {
+            data = typeof cachedData === "string" ? JSON.parse(cachedData) : cachedData;
+        }
+    }
+
+    if (!data && fetchIfNotFound) {
+        data = await getSpreadsheetData(spreadsheetId);
+        cache && await cache.set(`spreadsheet:${spreadsheetId}`, JSON.stringify(data), 5 * 60 * 1000)
+    }
+
+    if (data && typeof filterData === "function") {
+        data = filterData(data);
+    }
+
+    return data;
+}
+
+module.exports = {
+    getSpreadsheetData,
+    getCachedSpreadsheetData,
 }
