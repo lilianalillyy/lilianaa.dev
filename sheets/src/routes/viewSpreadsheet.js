537265124c4c3761d07e4b55ed49e81489b205ee
Mia Pilchová <valisova@lilianaa.dev>
2024-03-20 09:02:18 +0000

    refactor(sheets): use `getCachedSpreadsheetData` in view

--- sheets/src/routes/viewSpreadsheet.js
+++ sheets/src/routes/viewSpreadsheet.js
@@ -1,29 +1,15 @@
 const { authOrRedirect } = require("../auth");
 const fp = require("fastify-plugin");
-const { AbstractCachePromise } = require("../cache");
-const { getSpreadsheetData } = require("../spreadsheet");
+const { getCachedSpreadsheetData, getSpreadsheetData } = require("../spreadsheet");
 
 module.exports.viewSpreadsheet = fp(async (app) => {
-    const cache = app.cache ? new AbstractCachePromise(app.cache) : null;
-
     app.get('/spreadsheet/:spreadsheetId', authOrRedirect(async (req, res) => {
         try {
             const { spreadsheetId } = req.params;
 
-            if (cache) {
-                let cachedData = await cache.get(`spreadsheet:${spreadsheetId}`);
-
-                if (cachedData) {
-                    cachedData = typeof cachedData === "string" ? JSON.parse(cachedData) : cachedData;
-                    return res.view("sheet", {
-                        ...cachedData,
-                        user: req.userInfo
-                    });
-                }
-            }
-
-            const data = await getSpreadsheetData(spreadsheetId);
-            cache && await cache.set(`spreadsheet:${spreadsheetId}`, JSON.stringify(data), 5 * 60 * 1000)
+            const data = app.cache 
+                ? await getCachedSpreadsheetData(app.cache, spreadsheetId) 
+                : await getSpreadsheetData(spreadsheetId);
 
             return res.view("sheet", {
                 ...data,
