3cc8ecf9ae1ca4b846b940671d7c14c09d6e04a9
Mia Pilchová <valisova@lilianaa.dev>
2024-03-20 09:03:22 +0000

    fix(sheets): remove old `AbstractCachePromise`

--- sheets/src/cache.js
+++ sheets/src/cache.js
@@ -24,31 +24,3 @@ module.exports.createAbstractCache = (driver) => {
         driver,
     })
 }
-
-module.exports.AbstractCachePromise = class AbstractCachePromise {
-    cache;
-
-    constructor(cache) {
-        this.cache = cache;
-    }
-
-    get(key) {
-        return this._promisify(this.cache.get, key);
-    }
-
-    set(key, value, timeToLive) {
-        return this._promisify(this.cache.get, key, value, timeToLive);
-    }
-
-    _promisify(fn, ...params) {
-        return new Promise((resolve, reject) => {
-            fn(...params, (err, result) => {
-                if (err) {
-                    reject(err);
-                }
-
-                resolve(result);
-            })
-        })
-    }
-}
